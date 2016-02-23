3571c926301a582f8624309ac70688e94947f85a
Wasay Syed <wasay@witinc.com>
2016-02-22 23:47:42 -0500

    view page updated x2

--- vagrant/catalog/catalog/views.py
+++ vagrant/catalog/catalog/views.py
@@ -143,14 +143,6 @@ def deleteItem(category_name, item_title):
 
 
 
-
-
-
-
-# client id: # 842680508954-kao591b2ctpg8k88adthkuj2jh5f9nj3.apps.googleusercontent.com
-# sec: q0Ekim7UrsI77RcCd5oYdYi3
-
-
 # Create anti-forgery state token
 @app.route('/login')
 def showLogin():
--- vagrant/catalog/instance/config.py
+++ vagrant/catalog/instance/config.py
@@ -4,3 +4,8 @@ SQLALCHEMY_ECHO = True
 SECRET_KEY = 'some_seckret_key'
 STRIPE_API_KEY = 'some_api_key'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///itemcatalog.db'
+
+
+
+# client id: # 842680508954-kao591b2ctpg8k88adthkuj2jh5f9nj3.apps.googleusercontent.com
+# sec: q0Ekim7UrsI77RcCd5oYdYi3
